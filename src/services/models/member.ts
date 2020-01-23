78b127bc83b86dd772e68b406cd71bab130bc89b
JooHyeongLee <godparty@naver.com>
2020-01-23 23:50:19 +0900

    mongo model typescript 스타일로 변환

--- src/services/models/member.ts
+++ src/services/models/member.ts
@@ -1,10 +1,46 @@
-import mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+import { Schema, model, Document, Model } from 'mongoose';
 
-var MemberSchema = new Schema({
-  email: String,
+// var Schema = mongoose.Schema;
+declare interface Info extends Document{
+  name?: string;
+  email: string;
+  password: string;
+  profile_url: string;
+  phone?: string;
+  message?: string;
+  course_enquiry?: string;
+  creation_date?: Date;
+}
+
+export interface MemberModel extends Model<Info> {};
+
+export class Member {
+
+  private _model: Model<Info>;
+
+  constructor() {
+      const schema =  new Schema({
+          name: { type: String, required: true },
+          email: { type: String, required: true },
+          phone: { type: String },
+          message: { type: String },
+          course_enquiry: { type: String },
+          creation_date: { type: Date, default: Date.now }
+      },{collection: 'member'});
+
+      this._model = model<Info>('Member', schema);
+  }
+
+  public get model(): Model<Info> {
+      return this._model
+  }
+}
+
+
+/* var MemberSchema = new Schema({
+  email: {type: String},
   password: String
 }, {collection: 'member'});
 
-var Member = mongoose.model("Member", MemberSchema);
-export {Member}
+var Member = mongoose.model<Info>("Member", MemberSchema); */
+// export {Member}
--- src/services/routes/login.ts
+++ src/services/routes/login.ts
@@ -2,22 +2,22 @@ import { Router, Request, Response } from "express";
 import { logger } from "../../utils/logger";
 import { Member } from "../models/member";
 
-
 const login = {
     login: {
         path: "/login",
         method: "post",
         handler: [
             async ({body, session}: Request, res: Response) => {
-                let info = await Member.findOne({
+                let member = await new Member();
+                let info = await member.model.findOne({
                     email: body.email,
                     password: body.password
-                });
+                })
                 if(info) {
                     logger.info(`[route]: /login`);
                     session!.isLogin = true;
                     res.status(200).send("success");
-                }
+                } 
             }
         ]
     }
