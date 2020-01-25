b17c11b347db573e9ae0c2f97142ad49ff88d223
JooHyeongLee <godparty@naver.com>
2020-01-25 18:15:46 +0900

    mongo model class 형식으로 변환 시 이슈점검(객체 중복생성되어 오류발생)

---
+++ src/services/models/chatting.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+var Schema = mongoose.Schema;
+declare interface Info extends Document{
+  title: string;
+  password?: string;
+  count: number;
+  types: string
+  creation_date?: Date;
+}
+
+// export interface ChattingModel extends Model<Info> {};
+
+/* export class Chatting {
+
+  private _model: Model<Info>;
+
+  constructor() {
+      const schema =  new Schema({
+          title: { type: String, required: true },
+          types: { type: String, required: true },
+          password: { type: String },
+          count: { type: Number },
+          creation_date: { type: Date, default: Date.now }
+      },{collection: 'chatting'});
+
+      this._model = model<Info>('Chatting', schema);
+  }
+
+  public get model(): Model<Info> {
+      return this._model
+  }
+} */
+
+var ChattingSchema = new Schema({
+  title: { type: String, required: true },
+  types: { type: String, required: true },
+  password: { type: String },
+  count: { type: Number },
+  creation_date: { type: Date, default: Date.now }
+}, {collection: 'chatting'});
+
+var Chatting = mongoose.model("Chatting", ChattingSchema); 
+export {Chatting}
---
+++ src/services/routes/chatting.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from "express";
+import { logger } from "../../utils/logger";
+import { Chatting } from '../models/chatting';
+
+
+const chatting = {
+    selectList : {
+        path: "/api/chat/selectList",
+        method: "get",
+        handler: [
+            async ({  }: Request, res: Response) => {
+                logger.info('[route] /api/chat/selectList');
+                // 채팅방 리스트 조회
+                let list = await Chatting.find({});
+                res.status(200).send(list);
+            }
+        ]
+    },
+    create: {
+        path: "/api/chat/create",
+        method: "post",
+        handler: [
+            async ({  }: Request, res: Response) => {
+                logger.info('[route] /api/chat/create');
+                // 채팅방 생성
+                res.status(200).send("create");
+            }
+        ]
+    }
+}
+
+export {chatting}
